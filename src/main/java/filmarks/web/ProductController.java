892c030d85cdf14124d24d3f2af1d9f0793d811e
shizukanaoki <38066319+shizukanaoki@users.noreply.github.com>
2018-06-27 14:18:41 +0900

    映画一覧を表示

--- src/main/java/filmarks/web/ProductController.java
+++ src/main/java/filmarks/web/ProductController.java
@@ -1,9 +1,13 @@
 package filmarks.web;
 
 import filmarks.dbflute.exbhv.ProductBhv;
+import filmarks.dbflute.exentity.Product;
+import org.dbflute.cbean.result.ListResultBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ProductController {
@@ -11,7 +15,11 @@ public class ProductController {
     ProductBhv productBhv;
 
     @RequestMapping("/products")
-    public String index() {
-        return "product/index";
+    @ResponseBody
+    public ModelAndView index(ModelAndView mav) {
+        ListResultBean<Product> products = productBhv.selectList(cb -> cb.query().addOrderBy_Id_Asc());
+        mav.addObject("products", products);
+        mav.setViewName("product/index");
+        return mav;
     }
 }
