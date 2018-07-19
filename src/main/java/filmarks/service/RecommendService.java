eeca8a2bd45bd968eb55893def0850965a77bd98
shizukanaoki <naokishizuka14@gmail.com>
2018-07-19 15:38:51 +0900

    オススメ機能作成

---
+++ src/main/java/filmarks/service/RecommendService.java
@@ -0,0 +1,91 @@
+package filmarks.service;
+
+import filmarks.dbflute.exbhv.ArtistBhv;
+import filmarks.dbflute.exbhv.FavoriteBhv;
+import filmarks.dbflute.exbhv.UserBhv;
+import filmarks.dbflute.exentity.Artist;
+import filmarks.dbflute.exentity.Favorite;
+import filmarks.dbflute.exentity.User;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
+import org.dbflute.cbean.result.ListResultBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class RecommendService {
+
+    @Autowired
+    UserBhv userBhv;
+
+    @Autowired
+    ArtistBhv artistBhv;
+
+    @Autowired
+    FavoriteBhv favoriteBhv;
+
+    public List<Artist> findRecommendedArtists(User targetUser) {
+        ListResultBean<User> users = userBhv.selectList(userCB -> {
+            userCB.query().addOrderBy_UserId_Asc();
+        });
+
+        userBhv.loadFavorite(users, refCB -> {
+            refCB.setupSelect_Album();
+        });
+
+        ListResultBean<Favorite> favorites = favoriteBhv.selectList(favoriteCB -> {
+            favoriteCB.setupSelect_Album();
+        });
+
+        List<Integer> favoritedArtistIds = favorites.stream()
+                .map(favorite -> favorite.getAlbum().get().getArtistId())
+                .distinct()
+                .collect(Collectors.toList());
+
+        ListResultBean<Artist> artists = artistBhv.selectList(artistCB -> {
+            artistCB.query().addOrderBy_ArtistId_Asc();
+            artistCB.query().setArtistId_InScope(favoritedArtistIds);
+        });
+
+        double[][] matrixData = new double[users.size()][artists.size()];
+        double[][] targetUserFavoriteData = new double[1][artists.size()];
+
+        for (int i = 0; i < users.size(); i++) {
+            double[] userFavoriteData = new double[artists.size()];
+            User user = users.get(i);
+            List<Favorite> favoriteList = user.getFavoriteList();
+            for (int j = 0; j < artists.size(); j++) {
+                Artist artist = artists.get(j);
+                double number_of_favorites = favoriteList.stream()
+                        .map(favorite -> favorite.getAlbum().get().getArtistId())
+                        .filter(artistId -> artistId.equals(artist.getArtistId()))
+                        .count();
+                userFavoriteData[j] = number_of_favorites;
+            }
+            if (user.equals(targetUser)) {
+                targetUserFavoriteData[0] = userFavoriteData;
+            }
+            matrixData[i] = userFavoriteData;
+        }
+
+        RealMatrix corrMatrix = new PearsonsCorrelation().computeCorrelationMatrix(matrixData);
+        RealMatrix userFavoriteMatrix = MatrixUtils.createRealMatrix(targetUserFavoriteData);
+        RealMatrix scores = userFavoriteMatrix.multiply(corrMatrix);
+
+        Map<Artist, Double> map = new HashMap<>();
+        for (int k = 0; k < artists.size(); k++) {
+            map.put(artists.get(k), scores.getEntry(0, k));
+        }
+
+        int numberOfRecommendedArtist = 5;
+        return map.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList())
+                .subList(0, numberOfRecommendedArtist - 1);
+    }
+}
--- src/main/java/filmarks/web/AlbumController.java
+++ src/main/java/filmarks/web/AlbumController.java
@@ -5,13 +5,16 @@ import filmarks.dbflute.exbhv.AlbumBhv;
 import filmarks.dbflute.exbhv.FavoriteBhv;
 import filmarks.dbflute.exbhv.UserBhv;
 import filmarks.dbflute.exentity.Album;
+import filmarks.dbflute.exentity.Artist;
 import filmarks.dbflute.exentity.Favorite;
 import filmarks.dbflute.exentity.User;
+import filmarks.service.RecommendService;
 import filmarks.web.form.CommentForm;
 import org.dbflute.cbean.result.ListResultBean;
 import org.dbflute.optional.OptionalEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +23,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class AlbumController {
@@ -32,17 +38,25 @@ public class AlbumController {
     @Autowired
     FavoriteBhv favoriteBhv;
 
+    @Autowired
+    RecommendService recommendService;
+
     @RequestMapping(value = {"/", "/albums"})
     @ResponseBody
-    public ModelAndView index(ModelAndView mav) {
-        ListResultBean<Album> albums = albumBhv.selectList(cb -> cb.query().addOrderBy_AlbumId_Asc());
+    public ModelAndView index(ModelAndView mav, @AuthenticationPrincipal User user) {
+        List<Artist> recommendedArtists = recommendService.findRecommendedArtists(user);
+        List<Integer> artistIds = recommendedArtists.stream()
+                .map(Artist::getArtistId)
+                .collect(Collectors.toList());
+        ListResultBean<Album> albums = albumBhv.selectList(cb -> {
+            cb.query().setArtistId_InScope(artistIds);
+        });
+        Collections.shuffle(albums);
         albumBhv.loadComment(albums, cb -> {});
         mav.addObject("albums", albums);
         mav.setViewName("album/index");
         return mav;
     }
-
-
 
     @RequestMapping("/albums/{albumId}")
     @ResponseBody
