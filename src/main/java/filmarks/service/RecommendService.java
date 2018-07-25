811e91b3cf94c7006c597aa8fc77dd5ec623c0b0
shizukanaoki <naokishizuka14@gmail.com>
2018-07-25 18:25:05 +0900

    artistIdがnullのアルバムでエラーが起きないように修正

--- src/main/java/filmarks/service/RecommendService.java
+++ src/main/java/filmarks/service/RecommendService.java
@@ -43,6 +43,7 @@ public class RecommendService {
 
         List<Integer> favoritedArtistIds = favorites.stream()
                 .map(favorite -> favorite.getAlbum().get().getArtistId())
+                .filter(artistId -> artistId != null)
                 .distinct()
                 .collect(Collectors.toList());
 
